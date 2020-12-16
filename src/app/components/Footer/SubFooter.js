ff4843e14ada9c1a0120667a620dd6cf78acdff9
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-16 16:56:17 +0100

    Cleanup

--- src/app/components/Footer/SubFooter.js
+++ src/app/components/Footer/SubFooter.js
@@ -1,24 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SubFooter extends Component {
   render() {
     return (
-      <div className='mt-2'>
-        <div className='mt-5 mb-2 p-4 p-lg-5 d-flex align-items-center flex-column justify-content-center' style={{backgroundColor: '#f4f7fc'}}>
-          <img src='/media/logo.png' width={35} alt="logo"/>
+      <div className="mt-2">
+        <div
+          className="mt-5 mb-2 p-4 p-lg-5 d-flex align-items-center flex-column justify-content-center"
+          style={{ backgroundColor: '#f4f7fc' }}
+        >
+          <img src="/media/logo.png" width={35} alt="logo" />
           <div className="social-icons">
-            <i className="fa fa-facebook" aria-hidden="true"></i>
-            <i className="fa fa-twitter" aria-hidden="true"></i>
-            <i className="fa fa-pinterest-p" aria-hidden="true"></i>
-            <i className="fa fa-dribbble" aria-hidden="true"></i>
-            <i className="fa fa-rss" aria-hidden="true"></i>
+            <i className="fa fa-facebook" aria-hidden="true" />
+            <i className="fa fa-twitter" aria-hidden="true" />
+            <i className="fa fa-pinterest-p" aria-hidden="true" />
+            <i className="fa fa-dribbble" aria-hidden="true" />
+            <i className="fa fa-rss" aria-hidden="true" />
           </div>
         </div>
-        <div className='my-3 p-1 p-lg-2 small d-flex align-items-center justify-content-center'>
+        <div className="my-3 p-1 p-lg-2 small d-flex align-items-center justify-content-center">
           <span>&copy; samzwa25 . All rights reserved. </span>
         </div>
       </div>
-
     );
   }
 }
--- src/app/components/HomePage/HomePageBody/PageFilters/Tweets/TweetsList.js
+++ src/app/components/HomePage/HomePageBody/PageFilters/Tweets/TweetsList.js
@@ -1,20 +1,46 @@
-import React, {Component} from 'react';
-import TweetItem from "./TweetItem";
-class TweetsList extends Component {
-  render() {
-    //Get Tweets Data here
-    return (
-        <div>
-          <h5 className='py-2 py-lg-4 mt-3 mt-lg-5 h5'>Recent Tweets</h5>
-          {/* Map the Tweets data from array of objects*/}
-          <TweetItem userImage = '/media/card-img.png' userName = 'Jhon Doe'  userAccount= '@tweetdoe' postedAt = '1h' tweetDes = 'How does it feel, to be on your own'/>
-          <TweetItem userImage = '/media/card-img.png' userName = 'Jhon Doe'  userAccount= '@tweetdoe' postedAt = '1h' tweetDes = 'How does it feel, to be on your own'/>
-          <TweetItem userImage = '/media/card-img.png' userName = 'Jhon Doe'  userAccount= '@tweetdoe' postedAt = '1h' tweetDes = 'How does it feel, to be on your own'/>
-          <TweetItem userImage = '/media/card-img.png' userName = 'Jhon Doe'  userAccount= '@tweetdoe' postedAt = '1h' tweetDes = 'How does it feel, to be on your own'/>
-          <button className='btn text-uppercase small font-weight-bold btn-secondary w-100 rounded-0 p-3'>Follow</button>
-        </div>
-    );
-  }
-}
+import React from 'react';
+import TweetItem from './TweetItem';
+
+const TweetsList = () => (
+  // Get Tweets Data here
+  <div>
+    <h5 className="py-2 py-lg-4 mt-3 mt-lg-5 h5">Recent Tweets</h5>
+    {/* Map the Tweets data from array of objects */}
+    <TweetItem
+      userImage="/media/card-img.png"
+      userName="Jhon Doe"
+      userAccount="@tweetdoe"
+      postedAt="1h"
+      tweetDes="How does it feel, to be on your own"
+    />
+    <TweetItem
+      userImage="/media/card-img.png"
+      userName="Jhon Doe"
+      userAccount="@tweetdoe"
+      postedAt="1h"
+      tweetDes="How does it feel, to be on your own"
+    />
+    <TweetItem
+      userImage="/media/card-img.png"
+      userName="Jhon Doe"
+      userAccount="@tweetdoe"
+      postedAt="1h"
+      tweetDes="How does it feel, to be on your own"
+    />
+    <TweetItem
+      userImage="/media/card-img.png"
+      userName="Jhon Doe"
+      userAccount="@tweetdoe"
+      postedAt="1h"
+      tweetDes="How does it feel, to be on your own"
+    />
+    <button
+      type="button"
+      className="btn text-uppercase small font-weight-bold btn-secondary w-100 rounded-0 p-3"
+    >
+      Follow
+    </button>
+  </div>
+);
 
 export default TweetsList;
