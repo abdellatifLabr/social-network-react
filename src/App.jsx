08a9dd2c9c8de088ffcf18e070d1a79d5a3c304b
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-16 19:47:14 +0100

    Apollo client setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import Routes from './app/Routes';
+import { ApolloProvider } from '@apollo/client';
 
+import apolloClient from './graphql';
+import Routes from './app/Routes';
 import Header from './app/components/Header';
 
 function App() {
   return (
-    <React.Suspense fallback={() => <div>Loading...</div>}>
-      <BrowserRouter>
-        <Container className="py-4 px-lg-5">
-          <Header />
-          <Routes />
-        </Container>
-      </BrowserRouter>
-    </React.Suspense>
+    <ApolloProvider client={apolloClient}>
+      <React.Suspense fallback={() => <div>Loading...</div>}>
+        <BrowserRouter>
+          <Container className="py-4 px-lg-5">
+            <Header />
+            <Routes />
+          </Container>
+        </BrowserRouter>
+      </React.Suspense>
+    </ApolloProvider>
   );
 }
 
