7fb4d4066cfbe6097758314db1fbb775c7ff48e0
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-17 19:18:27 +0100

    Implement react redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { ApolloProvider } from '@apollo/client';
+import { Provider } from 'react-redux';
 
 import apolloClient from './graphql';
+import reduxStore from './store';
 import Routes from './app/Routes';
 import Header from './app/components/Header';
 
 function App() {
   return (
-    <ApolloProvider client={apolloClient}>
-      <React.Suspense fallback={() => <div>Loading...</div>}>
-        <BrowserRouter>
-          <Container className="py-4 px-lg-5">
-            <Header />
-            <Routes />
-          </Container>
-        </BrowserRouter>
-      </React.Suspense>
-    </ApolloProvider>
+    <Provider store={reduxStore}>
+      <ApolloProvider client={apolloClient}>
+        <React.Suspense fallback={() => <div>Loading...</div>}>
+          <BrowserRouter>
+            <Container className="py-4 px-lg-5">
+              <Header />
+              <Routes />
+            </Container>
+          </BrowserRouter>
+        </React.Suspense>
+      </ApolloProvider>
+    </Provider>
   );
 }
 
