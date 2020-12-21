62e9fb9657a15e93fc9f453a620515a1670237c5
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-21 19:57:05 +0100

    Add profile page user cover

--- src/app/pages/ProfilePage.jsx
+++ src/app/pages/ProfilePage.jsx
@@ -27,7 +27,12 @@ export default function ProfilePage() {
     user && (
       <Row>
         <Col md={12} className="text-center mb-5">
-          <Image src={user.image || '/media/avatar-img.png'} width="200" />
+          <Image src={user.cover || '/media/banner.png'} className="w-100" />
+          <Image
+            src={user.image || '/media/avatar-img.png'}
+            width="200"
+            style={{ marginTop: '-120px' }}
+          />
           <h1 className="mt-3 mb-1">{user.fullName}</h1>
           <small>@{user.username}</small>
         </Col>
