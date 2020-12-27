c310e72c1612e1b8f82b3aea7ab6504225301dae
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-27 11:52:29 +0100

    Make bootstrap consistent with design

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory, Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 
 export default function PostCard({ post }) {
   const history = useHistory();
@@ -12,18 +12,24 @@ export default function PostCard({ post }) {
 
   return (
     <Card style={{ cursor: 'pointer' }}>
+      <Card.Header>
+        <div className="d-flex align-items-center">
+          <div className="mr-2">
+            <Image src={user.image || '/media/avatar-img.png'} width="35" />
+          </div>
+          <div className="flex-grow-1 small">
+            <Link to={`/profile/${user.id}`} className="d-block">
+              {user.fullName}
+            </Link>
+            <span className="text-secondary">{createdSince} ago</span>
+          </div>
+        </div>
+      </Card.Header>
       <Card.Img onClick={() => gotoPost(id)} variant="top" src={imageUrl} />
       <Card.Body onClick={() => gotoPost(id)}>
         <Card.Title className="my-0">{title}</Card.Title>
         <Card.Text>{summary}</Card.Text>
       </Card.Body>
-      <Card.Footer>
-        <small>
-          Posted by <Link to={`/profile/${user.id}`}>{user.fullName}</Link>
-          <br />
-          <span className="text-secondary">{createdSince}</span>
-        </small>
-      </Card.Footer>
     </Card>
   );
 }
