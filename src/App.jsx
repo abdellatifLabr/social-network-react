bd1bd5659f623a599cc48b476ac484dc9087cbfd
Abdellatif <abdellatif1898@gmail.com>
2021-10-12 22:02:56 +0100

    Add providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import Routes from './app/Routes';
-import Header from './app/components/Header';
-import Menu from './app/components/Menu';
-import Footer from './app/components/Footer';
-import Loading from './app/components/Loading';
+import Routes from "./app/Routes";
+import Header from "./app/components/Header";
+import Menu from "./app/components/Menu";
+import Footer from "./app/components/Footer";
+import Loading from "./app/components/Loading";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#212529",
+    },
+    secondary: {
+      main: "#0056b3",
+    },
+  },
+});
+
+const queryClient = new QueryClient();
 
 function App() {
   const user = useSelector((state) => state.user);
 
   return (
-    <React.Suspense fallback={() => <Loading />}>
-      <BrowserRouter>
-        <Container className="py-4 px-lg-5">
-          <Header />
-          {user && <Menu />}
-          <Routes />
-          <Footer />
-        </Container>
-      </BrowserRouter>
-    </React.Suspense>
+    <QueryClientProvider client={queryClient}>
+      <React.Suspense fallback={() => <Loading />}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Container className="py-4 px-lg-5">
+              <Header />
+              {user && <Menu />}
+              <Routes />
+              <Footer />
+            </Container>
+          </ThemeProvider>
+        </BrowserRouter>
+      </React.Suspense>
+    </QueryClientProvider>
   );
 }
 
